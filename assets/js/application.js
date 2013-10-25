77763a2c4675bc67aa534391f56beea2249641ff
kyleledbetter <kyle@kyleledbetter.com>
2013-10-24 19:06:51 -0500

    Initial commit

---
+++ assets/js/application.js
@@ -0,0 +1,28 @@
+$(function(){
+	// sidebar Toggle
+	$(".sidebar-toggle").click(function(){
+		$(".sidebar").toggleClass("active");
+		$(".content").toggleClass("content-sidebar");
+		$("body").toggleClass("body-sidebar");
+		$(".header").toggleClass("header-sidebar");
+		return false;
+	});
+	$('.sidebar').bind('clickoutside', function (event) {
+		$(".sidebar").removeClass("active");
+	});
+	// aside Toggle
+	$(".aside-toggle").click(function(){
+		$(".aside").toggleClass("active");
+		$(".content").toggleClass("content-aside");
+		$("body").toggleClass("body-aside");
+		$(".header").toggleClass("header-aside");
+		return false;
+	});
+	$('.aside').bind('clickoutside', function (event) {
+		$(".aside").removeClass("active");
+	});
+	// Activate tooltip
+    $("[data-toggle='tooltip']").tooltip();
+    // Activate Popover
+    $("[data-toggle='popover']").popover({ trigger: "hover",container: "body" });
+});
